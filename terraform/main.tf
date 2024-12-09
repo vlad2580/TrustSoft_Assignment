e2325d21d73bd117497f3bac3d78bcc044fe7e2c
Vladislav Yurikov <yurikov.v@tutanota.com>
2024-12-09 09:40:59 +0100

    Subnet created

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,14 @@ resource "aws_vpc" "main" {
     Name = "MainVPC"
   }
 }
+
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.region
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
