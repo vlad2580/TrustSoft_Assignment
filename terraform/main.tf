5cd08632e345866a5b93352fd1991b966c267fa1
Vladislav Yurikov <yurikov.v@tutanota.com>
2024-12-09 11:08:54 +0100

    naming changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,17 +22,61 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "MainVPC"
+    Name = "vpc_internship_vladislav"
   }
 }
 
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "publicSB" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = var.region
+  availability_zone       = "eu-west-1a"
   tags = {
-    Name = "PublicSubnet"
+    Name = "publicSubnet_internship_vladislav"
   }
+}
+
+#---------------------------------------------------#
+# Security Group                                    #
+#---------------------------------------------------#
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_internship_vladislav"
+  }
+}
+
+
+#---------------------------------------------------#
+# EC2 instance                                      #
+#---------------------------------------------------#
+
+resource "aws_instance" "web_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.publicSB.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = "webServer__internship_vladislav"
+  }
+
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,14 @@
 variable "region" {
   description = "AWS region for resources"
-  default     = "eu-west-3"
+  default     = "eu-west-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.micro"
-} 
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance -  Ubuntu"
+  default     = "ami-0e9085e60087ce171"
+}
