685545fd791d32adfee0ca31ce0b233891161dfd
Vladislav Yurikov <yurikov.v@tutanota.com>
2024-12-09 11:30:09 +0100

    Private subnets creation & 2 EC2 instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,18 +37,38 @@ resource "aws_subnet" "publicSB" {
   }
 }
 
+resource "aws_subnet" "privateSB1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "eu-west-1a"
+  tags = {
+    Name = "privateSubnet1_internship_vladislav"
+  }
+}
+
+resource "aws_subnet" "privateSB2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "eu-west-1b"
+  tags = {
+    Name = "privateSubnet2_internship_vladislav"
+  }
+}
+
 #---------------------------------------------------#
 # Security Group                                    #
 #---------------------------------------------------#
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+  name        = "sg_internship_vladislav"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -71,12 +91,12 @@ resource "aws_security_group" "allow_tls" {
 #---------------------------------------------------#
 
 resource "aws_instance" "web_server" {
+  count                  = 2
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.publicSB.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
-    Name = "webServer__internship_vladislav"
+    Name = "webServer${count.index + 1}_internship_vladislav"
   }
-
 }
